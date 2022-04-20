5b3b56181d76697269a3604a854f0b31b5aad62c
Antonio Ruggiero <antonrugg@gmail.com>
2022-04-20 13:02:55 +0200

    init commit - created components structure init

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    prova
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+
+
+</style>
